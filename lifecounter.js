741b10aae73563fd9fa11776c748b1b6551b0d07
hangorazvan <58172000+hangorazvan@users.noreply.github.com>
2020-05-22 19:00:58 +0300

    Add files via upload

--- lifecounter.js
+++ lifecounter.js
@@ -8,7 +8,9 @@ Module.register("lifecounter", {
 	defaults: {
 		birthday: "1970-12-31 12:00:00",
 		counter: "seconds", // seconds, minutes, hours, months, weeks, days, years
-		comment: " seconds from my birthday", // your comment
+		before: "Has been", // your comment
+		after: "seconds of your life", // your comment
+		cssclass: "small",
 	},
 	
 	getScripts: function() {return ["moment.js"];},
@@ -22,10 +24,22 @@ Module.register("lifecounter", {
 	},
 
 	getDom: function() {
-		var lifecounter = moment().diff(this.config.birthday, this.config.counter) + this.config.comment;
 		var wrapper = document.createElement("div");
-		wrapper.className = "normal ssmall";
-		wrapper.innerHTML = lifecounter.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+		var yourtime = moment().diff(this.config.birthday, this.config.counter);
+		var lifecounter = this.config.before + " " + yourtime + " " + this.config.after;
+
+		if (yourtime > 499395200 && yourtime < 1000086400){ // one week before and one day after
+		    wrapper.className = "bright " + this.config.cssclass;
+		} else {
+		    wrapper.className = "normal " + this.config.cssclass;
+		}
+
+		if (this.config.decimalSymbol == "."){
+		    wrapper.innerHTML = lifecounter.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+		} else {
+		    wrapper.innerHTML = lifecounter.replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+		}
+		
 		return wrapper;
 	}
 });
