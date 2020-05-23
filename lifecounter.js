38c3fcb73a4b317872472cb59bc621d1d000e36d
Razvan Cristea <58172000+hangorazvan@users.noreply.github.com>
2020-05-23 08:59:12 +0300

    Add files via upload

--- lifecounter.js
+++ lifecounter.js
@@ -6,7 +6,7 @@
 
 Module.register("lifecounter", {
 	defaults: {
-		birthday: "1988-12-31 12:00:00", // year, month, day, 24 hour birthday time
+		birthday: "1970-12-31 12:00:00", // year, month, day, 24 hour birthday time
 		counter: "seconds", // seconds, minutes, hours, months, weeks, days, years
 		before: "Has been", // your comment
 		after: "seconds of your life", // your comment
@@ -28,7 +28,7 @@ Module.register("lifecounter", {
 		var yourtime = moment().diff(this.config.birthday, this.config.counter);
 		var lifecounter = this.config.before + " " + yourtime + " " + this.config.after;
 
-		if (yourtime > 999395200 && yourtime < 1000086400){ // one week before and one day after
+        if (yourtime > 999395200 && yourtime < 1000086400) { // one week before and one day after
 		    wrapper.className = "bright " + this.config.cssclass;
 		} else {
 		    wrapper.className = "normal " + this.config.cssclass;
